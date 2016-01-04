c6554f4c601132719402b3948b9b33aee3eb3800
Quicks <1989alpan@gmail.com>
2016-01-04 21:48:42 +0200

    cache

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
 	validates :title, :description , :image_url, presence: true
+	validates :title, length: {minimum:10}
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness:true
 	validates :image_url, allow_blank: true, format:
@@ -7,4 +8,8 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'Url має вказувати на картинку у форматі GIF,jpg,png'
 	}
+
+	def self.latest
+		# Product.order(:updated_at).last
+	end	
 end
