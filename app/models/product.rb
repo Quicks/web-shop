b9d5d28f817f746bed6a9a6e50597a479e9966b5
Quicks <1989alpan@gmail.com>
2016-01-07 21:23:58 +0200

    Ajax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
+
 	validates :title, :description , :image_url, presence: true
 	validates :title, length: {minimum:10}
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -9,7 +14,20 @@ class Product < ActiveRecord::Base
 		message: 'Url має вказувати на картинку у форматі GIF,jpg,png'
 	}
 
+
+
+
 	def self.latest
 		# Product.order(:updated_at).last
 	end	
+
+	private
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+			errors(:base,'існують товарні позиції')
+			return false	
+		end
+	end
 end
